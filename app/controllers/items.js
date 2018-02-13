13edbefa8c7c9fd2de4dcc57b4db83c35a1c7035
briankeegan <brian.morris.keegan@gmail.com>
2018-02-13 15:00:32 -0500

    Adjust item destroy curl script
    Adjust item destroy curl script, add the authentication header
    and received success message in terminal.
    Ensure that unauthenticated user cannot delete item and users can only
    delete their own items.
    
    @quidprocrow
      @briankeegan
        @smb2255

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -61,7 +61,7 @@ module.exports = controller({
   destroy
 }, { before: [
   { method: setUser, only: ['index', 'show'] },
-  { method: authenticate, except: ['index', 'show', 'destroy', 'update'] },
+  { method: authenticate, except: ['index', 'show', 'update'] },
   { method: setModel(Item), only: ['show'] },
   { method: setModel(Item, { forUser: true }), only: ['update', 'destroy'] }
 ] })
--- scripts/items/destroy.sh
+++ scripts/items/destroy.sh
@@ -6,5 +6,6 @@ URL_PATH="/items"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request DELETE \
+  --header "Authorization: Token token=$TOKEN" \
 
 echo
