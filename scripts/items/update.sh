da1ab4f933e36b20bdb2e1b3b794cdd39ef6b1d4
quidprocrow <windmillwarrior@gmail.com>
2018-02-16 10:08:32 -0500

    Add updated curl scripts
    Add updated curl scripts for user and item testing
    
    @quidprocrow
    @briankeegan
    @smb2255

--- scripts/items/update.sh
+++ scripts/items/update.sh
@@ -2,10 +2,10 @@
 
 API="http://localhost:4741"
 URL_PATH="/items"
-
+TOKEN="WF12X+MoUcLZ/YBLKJKuH7kNP5brHqEztWLVCBoUWwc=--SzjwBguNTmSxcfTzja9fnem3qq7v4XS/+H1DJuTaFJA="
 TITLE="A FREAKING NEW TITLE"
 DESC="AN EVEN BETTER DESCRIPTION"
-ID="5a83691298bd3325d70dab17"
+ID="5a837a48e9f3b92d95ce27d5"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
